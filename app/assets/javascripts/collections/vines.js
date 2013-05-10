db38c78edd29115715a4b50a5bcc8234c33bbd45
Nate Hayflick <nhayflick@gmail.com>
2013-05-09 19:18:57 -0700

    what

--- app/assets/javascripts/collections/vines.js
+++ app/assets/javascripts/collections/vines.js
@@ -9,16 +9,16 @@ VI.Collections.Vines = Backbone.Collection.extend({
     var query = "vine.co%20" + encodeURIComponent(querystring);
     var queryModel = new VI.Models.Query({body: querystring});
     queryModel.save();
-    $.getJSON("http:/search.twitter.com/search.json?callback=?",{
-      include_entities: "true",
-      q: query
-    }, function(data) {
-      // console.log(data);
-      setTimeout( function(){
-        that.addFromQuery(data.results, callback);
-      }, 150)
-    });
-  }
+  //   $.getJSON("http:/search.twitter.com/search.json?callback=?",{
+  //     include_entities: "true",
+  //     q: query
+  //   }, function(data) {
+  //     // console.log(data);
+  //     setTimeout( function(){
+  //       that.addFromQuery(data.results, callback);
+  //     }, 150)
+  //   });
+  // }
 
 //git push heroku production:master
 
@@ -28,19 +28,19 @@ VI.Collections.Vines = Backbone.Collection.extend({
 //   }
 // });
 
-  //       $.ajax({
-  //         url: 'https://search.twitter.com/search',
-  //         type: 'GET',
-  //         dataType: 'jsonp',
-  //         data: {
-  //           include_entities: 'true',
-  //           q: query
-  //         },
-  //         success: function() {that.addFromQuery(data.results, callback) },
-  //         error: function() { alert('vino is not pleased') },
-  //         // beforeSend: setHeader
-  //       });
-  // },
+        $.ajax({
+          url: 'https://search.twitter.com/search',
+          type: 'GET',
+          dataType: 'jsonp',
+          data: {
+            include_entities: 'true',
+            q: query
+          },
+          success: function() {that.addFromQuery(data.results, callback) },
+          error: function() { alert('vino is not pleased') },
+          // beforeSend: setHeader
+        });
+  },
 
   // setHeader: function(xhr) {
   //   xhr.setRequestHeader('User-Agent', 'Vino/0.5')
