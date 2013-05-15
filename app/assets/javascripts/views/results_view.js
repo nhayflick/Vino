7c761ec35e3cf4b90dfeeb3b4d9876e4d8e568b3
Nate Hayflick <nhayflick@gmail.com>
2013-05-15 13:09:56 -0700

    background changes

--- app/assets/javascripts/views/results_view.js
+++ app/assets/javascripts/views/results_view.js
@@ -32,14 +32,14 @@ VI.Views.ResultsView = Backbone.View.extend({
       video = $(".vine").first().find("video")
       that.playVine(video);
     }
-    }, 1000);
+    }, 3000);
     // Scrolls down to the first video
     setTimeout(function(){
       that.scrollHeight += ($(".content").offset().top);
       $("html,body").animate({ scrollTop: that.scrollHeight}, "slow");
       // Unhides the navbar
       $(".navbar").slideDown();
-    }, 1000);  
+    }, 2000);  
   },
 
   scrollToNext: function() {
